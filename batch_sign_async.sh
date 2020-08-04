b1d6e70eb0ccc50dd28cdba81bb9ad90329d24f8
umpint <robin.owens@umpint.com>
2020-08-04 12:34:04 +0100

    add in async api call

---
+++ batch_sign_async.sh
@@ -0,0 +1,88 @@
+# Copyright 2020 rowit Ltd.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+#
+# This is an example bash script that takes 3 parameters:
+# 1. The URL of the domain that is signing the files.
+# 2. The path to the private key that coresponds to the URL in (1)
+# 3. A path to a directory that contains a list of files to be signed
+# 
+# It will then upload all the hashes and signature to the umpint.com 
+# servers.
+#
+# It will finally print out a JSON result detailing status of each file 
+# it tried to sign and upload.
+
+if [ "$#" -ne 3 ]; then
+  echo "Please pass 3 arguments:"
+  echo "url certificatePrivateKeyPath directory"
+  exit 1
+fi
+
+url=$1
+privateKey=$2
+directory=$3
+
+if [ ! -r $privateKey ]; then
+  echo "could not read certificate Private Key : [$privateKey]"
+  exit 2
+fi
+
+grep "\-\-\-\-\-BEGIN PRIVATE KEY\-\-\-\-\-" $privateKey 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "key did on contain -----BEGIN PRIVATE KEY----- as expected - check format"
+  exit 3
+fi
+grep "\-\-\-\-\-END PRIVATE KEY\-\-\-\-\-" $privateKey 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "key did on contain -----END PRIVATE KEY----- as expected - check format"
+  exit 4
+fi
+
+if [ ! -r $directory ]; then
+  echo "could not read file to sign : [$file]"
+  exit 5
+fi
+
+
+first="yes"
+# This loop manually generates an JSON array of [hash, signatures].
+echo "[" > /tmp/post.$$.txt
+for file in `ls $directory`
+do
+  if [ "${first}" == "no" ]; then
+    echo "," >> /tmp/post.$$.txt
+  fi
+  first="no"
+  # compute hash of file as hex string and store in temp file (with no \n line erminator)
+  sha256sum $directory/$file | awk '{print $1}' | tr -d '\n' > /tmp/hash.$$.txt
+  # compute signature
+  openssl dgst -sha256 -sign $privateKey -out /tmp/sign.$$.bin /tmp/hash.$$.txt
+  # convert signature to base64
+  openssl base64 -in /tmp/sign.$$.bin -out /tmp/sign.$$.txt
+  hash=`cat /tmp/hash.$$.txt`
+  # convert base64 signature to url encoded base64 encoded signature
+  sig=`cat /tmp/sign.$$.txt | tr -d '\n' | tr -- '+=/' '-_~'| sed -e 's/ //g'`
+  echo "hash [$hash] sig [$sig]"
+  echo "[ \"$hash\", \"$sig\" ]" >> /tmp/post.$$.txt
+done
+echo "]" >> /tmp/post.$$.txt
+
+echo ""
+echo "Result:"
+# upload json array of arrays to umpint.com servers.
+curl -XPOST "https://umpint.com/api/v1/signasync/$url" --data-binary @/tmp/post.$$.txt  --header "Content-type: application/json"
+echo ""
+
+#rm /tmp/hash.$$.txt /tmp/sign.$$.txt /tmp/post.$$.txt
